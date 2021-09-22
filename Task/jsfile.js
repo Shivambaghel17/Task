fd9ba02b8e348e05867159bb0c1c928f52fbed67
Shivambaghel17 <72193273+Shivambaghel17@users.noreply.github.com>
2021-09-22 11:20:24 +0530

    Add files via upload

---
+++ Task/jsfile.js
@@ -0,0 +1,188 @@
+function clickhere(){
+    document.getElementById('inner1').style.backgroundColor='#DC7633';
+    document.getElementById('inner2').style.backgroundColor='';
+    document.getElementById('inner3').style.backgroundColor='';
+    document.getElementById('inner4').style.backgroundColor='';
+    document.getElementById('inner5').style.backgroundColor='';
+    console.log("Responsiveness-Extremely bad");
+}
+ 
+
+ function clickhere1(){
+    document.getElementById('inner3').style.backgroundColor='';
+    document.getElementById('inner4').style.backgroundColor='';
+    document.getElementById('inner5').style.backgroundColor='';
+   document.getElementById('inner1').style.backgroundColor='#DC7633';
+    document.getElementById('inner2').style.backgroundColor='#DC7633';
+    console.log("Responsiveness-Bad");
+
+}
+
+function clickhere2(){
+
+    document.getElementById('inner4').style.backgroundColor='';
+    document.getElementById('inner5').style.backgroundColor='';
+    document.getElementById('inner1').style.backgroundColor='#FFC300';
+    document.getElementById('inner2').style.backgroundColor='#FFC300';
+    document.getElementById('inner3').style.backgroundColor='#FFC300';
+    console.log("Responsiveness-Neutral");
+}
+
+function clickhere3(){
+    document.getElementById('inner5').style.backgroundColor='#8e9292 ';
+    document.getElementById('inner1').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner2').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner3').style.backgroundColor='#27AE60 ';
+    document.getElementById('inner4').style.backgroundColor='#27AE60 ';
+    console.log("Responsiveness-Good");
+}
+
+function clickhere4(){
+    document.getElementById('inner1').style.backgroundColor='#145A32';
+    document.getElementById('inner2').style.backgroundColor='#145A32 ';
+    document.getElementById('inner3').style.backgroundColor='#145A32 ';
+    document.getElementById('inner4').style.backgroundColor='#145A32 ';
+    document.getElementById('inner5').style.backgroundColor='#145A32 ';
+    console.log("Responsiveness-Extremely good");
+}
+
+function click_here(){
+    document.getElementById('inner_1').style.backgroundColor='#DC7633';
+    document.getElementById('inner_2').style.backgroundColor='';
+    document.getElementById('inner_3').style.backgroundColor='';
+    document.getElementById('inner_4').style.backgroundColor='';
+    document.getElementById('inner_5').style.backgroundColor='';
+    console.log("Competency-Extremely bad");
+}
+
+function click_here1(){
+    document.getElementById('inner_3').style.backgroundColor='';
+    document.getElementById('inner_4').style.backgroundColor='';
+    document.getElementById('inner_5').style.backgroundColor='';
+   document.getElementById('inner_1').style.backgroundColor='#DC7633';
+    document.getElementById('inner_2').style.backgroundColor='#DC7633';
+    console.log("Competency- Bad");
+}
+
+function click_here2(){
+
+    document.getElementById('inner_4').style.backgroundColor='';
+    document.getElementById('inner_5').style.backgroundColor='';
+    document.getElementById('inner_1').style.backgroundColor='#FFC300';
+    document.getElementById('inner_2').style.backgroundColor='#FFC300';
+    document.getElementById('inner_3').style.backgroundColor='#FFC300';
+    console.log("Competency-Neutral");
+}
+
+function click_here3(){
+    document.getElementById('inner_5').style.backgroundColor='#8e9292 ';
+    document.getElementById('inner_1').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner_2').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner_3').style.backgroundColor='#27AE60 ';
+    document.getElementById('inner_4').style.backgroundColor='#27AE60 ';
+    console.log("Competency-Good");
+}
+
+function click_here4(){
+    document.getElementById('inner_1').style.backgroundColor='#145A32';
+    document.getElementById('inner_2').style.backgroundColor='#145A32 ';
+    document.getElementById('inner_3').style.backgroundColor='#145A32 ';
+    document.getElementById('inner_4').style.backgroundColor='#145A32 ';
+    document.getElementById('inner_5').style.backgroundColor='#145A32 ';
+    console.log("Competency-Extremely good");
+}
+
+function click__here(){
+    document.getElementById('inner__1').style.backgroundColor='#DC7633';
+    document.getElementById('inner__2').style.backgroundColor='';
+    document.getElementById('inner__3').style.backgroundColor='';
+    document.getElementById('inner__4').style.backgroundColor='';
+    document.getElementById('inner__5').style.backgroundColor='';
+    console.log("Trustworthiness-Extremely bad");
+}
+
+function click__here1(){
+    document.getElementById('inner__3').style.backgroundColor='';
+    document.getElementById('inner__4').style.backgroundColor='';
+    document.getElementById('inner__5').style.backgroundColor='';
+   document.getElementById('inner__1').style.backgroundColor='#DC7633';
+    document.getElementById('inner__2').style.backgroundColor='#DC7633';
+    console.log("Trustworthiness- Bad");
+}
+
+function click__here2(){
+
+    document.getElementById('inner__4').style.backgroundColor='';
+    document.getElementById('inner__5').style.backgroundColor='';
+    document.getElementById('inner__1').style.backgroundColor='#FFC300';
+    document.getElementById('inner__2').style.backgroundColor='#FFC300';
+    document.getElementById('inner__3').style.backgroundColor='#FFC300';
+    console.log("Trustworthiness-Neutral");
+}
+
+function click__here3(){
+    document.getElementById('inner__5').style.backgroundColor='#8e9292 ';
+    document.getElementById('inner__1').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner__2').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner__3').style.backgroundColor='#27AE60 ';
+    document.getElementById('inner__4').style.backgroundColor='#27AE60 ';
+    console.log("Trustworthiness-Good");
+}
+
+function click__here4(){
+    document.getElementById('inner__1').style.backgroundColor='#145A32';
+    document.getElementById('inner__2').style.backgroundColor='#145A32 ';
+    document.getElementById('inner__3').style.backgroundColor='#145A32 ';
+    document.getElementById('inner__4').style.backgroundColor='#145A32 ';
+    document.getElementById('inner__5').style.backgroundColor='#145A32 ';
+    console.log("Trustworthiness-Extremely good");
+}
+function click___here(){
+    document.getElementById('inner___1').style.backgroundColor='#DC7633';
+    document.getElementById('inner___2').style.backgroundColor='';
+    document.getElementById('inner___3').style.backgroundColor='';
+    document.getElementById('inner___4').style.backgroundColor='';
+    document.getElementById('inner___5').style.backgroundColor='';
+    console.log("Customer services and support-Extremely bad");
+}
+
+function click___here1(){
+    document.getElementById('inner___3').style.backgroundColor='';
+    document.getElementById('inner___4').style.backgroundColor='';
+    document.getElementById('inner___5').style.backgroundColor='';
+   document.getElementById('inner___1').style.backgroundColor='#DC7633';
+    document.getElementById('inner___2').style.backgroundColor='#DC7633';
+    console.log("Customer services and support-Bad");
+}
+
+function click___here2(){
+
+    document.getElementById('inner___4').style.backgroundColor='';
+    document.getElementById('inner___5').style.backgroundColor='';
+    document.getElementById('inner___1').style.backgroundColor='#FFC300';
+    document.getElementById('inner___2').style.backgroundColor='#FFC300';
+    document.getElementById('inner___3').style.backgroundColor='#FFC300';
+    console.log("Customer services and support-Neutral");
+}
+
+function click___here3(){
+    document.getElementById('inner___5').style.backgroundColor='#8e9292 ';
+    document.getElementById('inner___1').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner___2').style.backgroundColor='#27AE60  ';
+    document.getElementById('inner___3').style.backgroundColor='#27AE60 ';
+    document.getElementById('inner___4').style.backgroundColor='#27AE60 ';
+    console.log("Customer services and support-Good");
+}
+
+function click___here4(){
+    document.getElementById('inner___1').style.backgroundColor='#145A32';
+    document.getElementById('inner___2').style.backgroundColor='#145A32 ';
+    document.getElementById('inner___3').style.backgroundColor='#145A32 ';
+    document.getElementById('inner___4').style.backgroundColor='#145A32 ';
+    document.getElementById('inner___5').style.backgroundColor='#145A32 ';
+    console.log("Customer services and support-Extremely good");
+}
+
+function reLoad(){
+    document.location.reload();
+}
